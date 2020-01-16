3b36d6492cebfa9d5c30fa582c228780bbaa4ce0
Dike Nnamaka <nnamaka7@gmail.com>
2020-01-16 23:36:42 +0100

    add to HugeInt.cpp source code

--- HugeInt.cpp
+++ HugeInt.cpp
@@ -27,3 +27,75 @@ HugeInt::HugeInt( const char * strg )
         if ( isdigit( strg[ k ] ))
             integer[ j ] = strg[ k ] - '0' ;
 }
+
+HugeInt HugeInt::operator+ ( const HugeInt &op2 ) const 
+{
+    HugeInt temp ;
+    int carry = 0 ;
+
+    for( int i = 29; i >= 0  ; i-- )
+    {
+        temp.integer[ i ] = integer[ i ] + op2.integer[i ] + carry ;
+
+        if( temp.integer[ i ] > 9 )
+        {
+            temp.integer[i] %= 10;
+            carry = 1 ;
+        }
+
+        else 
+        carry = 0 ;
+    }
+
+    return temp ;
+}
+
+HugeInt HugeInt::operator+( int op2 ) const 
+{
+    return *this + HugeInt( op2 ) ;
+}
+
+HugeInt HugeInt::operator+ ( const char * op2 ) const 
+{
+    return *this + HugeInt( op2 );
+}
+
+ostream &operator<< ( ostream &output , const HugeInt &num )
+{
+    int i ;
+    for ( i = 0 ; ( num.integer[ i ] == 0 ) && ( i <= 29) ; i++)
+    ;
+
+    if ( i == 30 )
+        output << 0 ;
+    else
+        {
+            for(; i <= 29 ; i++ )
+            output << num.integer[ i ] ;
+        }
+    return output ;
+    
+}
+
+bool HugeInt::operator< ( const HugeInt &right ) const
+{
+    for( int i = 0 ; i <= 29 ; i++ )
+    {
+        if ( integer[ i] == right.integer[i] )
+            continue ;
+        if( integer[i] > right.integer[i] )
+            return false ;
+        else
+            return true ;
+        
+    }
+    return false ;
+}
+
+bool HugeInt::operator==( const HugeInt &right ) const
+{
+    for( int i = 0 ; i <= 29; i++)
+        if( integer[i] != right.integer[i] )
+            return false ;
+    return true ;
+}
